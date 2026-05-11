e67660da49cace98798b8e41b82de6d088fa03b9
emilio <favoretoemilio@gmail.com>
2026-05-11 14:01:22 -0300

    teste de algumas rotas e correção do server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
-import exemplosRoutes from './routes/exemploRoute.js';
+import quizRoutes from './routes/quizRoute.js';
+import dicaRoutes from './routes/dicaRoute.js';
+import temasRedacaoRoutes from './routes/temasRedacaoRoute.js';
+import curiosidadesRoute from './routes/curiosidadesRoute.js'
+import usuarioRoutes from './routes/usuarioRoute.js';
+import livroRoutes from './routes/livroRoute.js'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +17,12 @@ app.get('/', (req, res) => {
 });
 
 // Rotas
-app.use('/api/exemplos', exemplosRoutes);
+app.use('/api/curiosidades', curiosidadesRoute);
+app.use('/api/dica', dicaRoutes);
+app.use('/api/livro', livroRoutes);
+app.use('/api/quiz', quizRoutes);
+app.use('/api/temasRedacao', temasRedacaoRoutes);
+app.use('/api/usuario', usuarioRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
