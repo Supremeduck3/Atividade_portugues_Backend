042f01593a45bc9bc10a114e3d355316f7ce9d1e
emilio <favoretoemilio@gmail.com>
2026-05-11 16:11:10 -0300

    teste de correcao

--- src/controllers/temasRedacaoController.js
+++ src/controllers/temasRedacaoController.js
@@ -1,4 +1,4 @@
-import TemasRedacaoModel from '../models/TemasRedacaoModel.js';
+import TemasRedacaoModel from '../models/temasRedacaoModel.js'
 
 export const criar = async (req, res) => {
     try {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
-import exemplosRoutes from './routes/exemploRoute.js';
+import curiosidadesRoutes from './routes/curiosidadesRoute.js';
+import dicaRoutes from './routes/dicaRoute.js';
+import livroRoutes from './routes/livroRoute.js';
+import quizsRoutes from './routes/quizRoute.js';
+import temasRedacaoRoutes from './routes/temasRedacaoRoute.js';
+import usuarioRoutes from './routes/usuarioRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +17,12 @@ app.get('/', (req, res) => {
 });
 
 // Rotas
-app.use('/api/exemplos', exemplosRoutes);
+app.use('/api/exemplos', curiosidadesRoutes);
+app.use('/api/exemplos', dicaRoutes);
+app.use('/api/exemplos', livroRoutes);
+app.use('/api/exemplos', quizsRoutes);
+app.use('/api/temasRedacao', temasRedacaoRoutes);
+app.use('/api/usuario', usuarioRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
