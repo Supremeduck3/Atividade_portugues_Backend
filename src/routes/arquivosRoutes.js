5a85bbefef28aae650fd61363e79097ccf1403b4
Supremeduck008 <pedro.otaviobraga@gmail.com>
2026-05-18 09:11:14 -0300

    altera o upload de imagens

--- src/routes/arquivosRoutes.js
+++ src/routes/arquivosRoutes.js
@@ -4,8 +4,14 @@ import { upload } from './../lib/middleware/fileGate.js';
 
 const router = express.Router();
 
+
 router.post('/:id/capa', upload.single('capa'), controller.uploadCapa);
 router.get('/:id/capa', controller.buscarCapa);
 router.delete('/:id/capa', controller.deletarCapa);
+
+router.post('/:id/Imagem', upload.single('capa'), controller.uploadImagem);
+router.get('/:id/Imagem', controller.buscarImagem);
+router.delete('/:id/Imagem', controller.deletarImagem);
+
 
 export default router;
