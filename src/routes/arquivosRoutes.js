1f44db9ee87f2d5b8cf5846ee76607a411961ef2
Supremeduck008 <pedro.otaviobraga@gmail.com>
2026-05-18 10:34:54 -0300

    adiciona imagens route

--- src/routes/arquivosRoutes.js
+++ src/routes/arquivosRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as controller from '../controllers/arquivoController.js';
+import * as imagemController from '../controllers/imagemController.js'
 import { upload } from './../lib/middleware/fileGate.js';
 
 const router = express.Router();
@@ -13,5 +14,9 @@ router.post('/:id/Imagem', upload.single('imagem'), controller.uploadImagem);
 router.get('/:id/Imagem', controller.buscarImagem);
 router.delete('/:id/Imagem', controller.deletarImagem);
 
-
+router.post('/', imagemController.criar);
+router.get('/', imagemController.buscarTodos);
+router.get('/:id', imagemController.buscarPorId);
+router.put('/:id', imagemController.atualizar);
+router.delete('/:id', imagemController.deletar);
 export default router;
